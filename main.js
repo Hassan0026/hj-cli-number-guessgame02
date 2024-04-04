272b0775f3d7ed316eb0999c75fcb0453fce08c8
Hassan0026 <159889462+Hassan0026@users.noreply.github.com>
2024-04-04 23:08:23 +0500

    added a console
    added a console

---
+++ main.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+// computer will generate a random number
+// user input for guessing number
+// compare user input with computer generator number & show result
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to number Guessing Game");
+console.log(randomnumber);
+export {};
+//const answers = await inquirer.prompt([
+//{
+//    name: "userGuessedNumber",
+//    type: "number",
+//    message: "Please guess a number between 1-6:",
+//},
+//]);
+//
+//if (answers.userGuessedNumber === randomnumber)
+//{   console.log("Congratulations! you guessed write number.");
+//}
+//else {console.log ("You guessed wrong number.");
+//}
